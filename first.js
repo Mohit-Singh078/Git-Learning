eedecf41d1f6b19a476791d02e7334a4d726fdab
Mohit Pratap Singh <mohitpersonal26@gmail.com>
2026-06-18 14:42:23 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,14 @@
+const button = "Added a button";
+console.log(button);
+
+const login = "Login page added";
+console.log(login);
+
+const footer = "Footer addded in our website";
+console.log(footer);
+
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
+
+const upi =  "Integrated the UPI";
+console.log(upi);
